860c457358ad650e221b908755580cf0428999b5
LI Chuan <lichuan172@gmail.com>
2023-08-29 10:43:54 +0800

    fix: fix bug in clear action

--- src/actions.ts
+++ src/actions.ts
@@ -2,17 +2,22 @@ import { execSync } from "child_process"
 
 export function am({ patchPath }: { patchPath: string }) {
   process.chdir("/verdaccio/storage")
-  execSync("git init")
-  execSync("git config user.name 'verdaccio'")
-  execSync("git config user.email 'verdaccio@email.com'")
-  execSync("git add .")
-  execSync('git commit -m "git commit before am"')
-  execSync(`git am -3 ${patchPath}`)
-}
-
-export function clear() {
-  process.chdir("/verdaccio/storage")
-  execSync("rm -rf ./.git/*")
-  execSync("git init")
-  execSync("rm -rf ./data/*")
-}
+  // execSync("git init")
+  // execSync("git config user.name 'verdaccio'")
+  // execSync("git config user.email 'verdaccio@email.com'")
+  // execSync("git add .")
+  // try {
+    //   execSync('git commit -m "git commit before am"')
+    // } catch {}
+    execSync(`git am -3 ${patchPath}`)
+  }
+  
+  export function clear() {
+    process.chdir("/verdaccio/storage")
+    execSync("rm -rf ./.git/*")
+    execSync("rm -rf ./data/*")
+    execSync("git init")
+    execSync("git config user.name 'verdaccio'")
+    execSync("git config user.email 'verdaccio@email.com'")
+  }
+  
